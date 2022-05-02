88c59c43444b5fee9b4ab521408c3bdc2ddaf198
Godkiller98 <64099566+Godkiller98@users.noreply.github.com>
2022-05-02 09:22:34 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,14 @@
+
+
+// // to reverse the string
+class t{
+  public static void main(String[] args) {
+    String s="Sonali"; String str="";
+    int n=s.length();
+    char ch;
+    for (int i=0;i<n ;i++ ) {
+       ch=s.charAt(i);
+      str=ch+str;
+    }System.out.println(str);
+  }
+}
